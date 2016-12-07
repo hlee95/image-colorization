Added correct scaling to Lab

network.py
@@ -47,12 +47,12 @@ def color_small():
 	#print(sess.run(accuracy, feed_dict={x: mnist.test.images.reshape(-1,28,28,1), y_: mnist.test.labels}))
 
 SEED = 66478 				# Set to None for random seed.
-NUM_TRAIN_IMAGES = 100000 		# Should be 100,000 for actual dataset.
+NUM_TRAIN_IMAGES = 5000 		# Should be 100,000 for actual dataset.
 NUM_TEST_IMAGES = 100 		# Should be 10,000 for actual dataset.
 NUM_VAL_IMAGES = 100			# Should be 10,000 for actual dataset.
 BATCH_SIZE = 100 			# Should be 128 for actual dataset.
 EVAL_BATCH_SIZE = 100
-EVAL_FREQUENCY = 100			# Subject to change...
+EVAL_FREQUENCY = 5			# Subject to change...
 IMAGE_SIZE = 128
 DEPTH = 64 				# Used to parameterize the depth of each output layer.
 FINAL_DEPTH = 2 			# The final depth should always be 2.
@@ -63,6 +63,8 @@ IMAGES_DIR = 'data/images/' 		# Relative or absolute path to directory where ima
                  			# IMAGES_DIR should have 3 subdirectories: train, val, test
 CKPT_DIR = './new_ckpt_dir'
 NUM_EPOCHS = 1
+AB_MAX = 127				# For scaling a and b color channel values to between 0 and 1.
+AB_MIN = -128
 
 def read_scaled_color_image_Lab(filename):
 	# Read image, cut off alpha channel, only keep rgb.
@@ -71,29 +73,9 @@ def read_scaled_color_image_Lab(filename):
 	rgb = misc.imresize(rgb, (128,128))
 	lab = color.rgb2lab(rgb).astype(np.float32)
 	# rescale a and b so that they are in the range (0,1) of the sigmoid function
-	a_min = np.min(lab[:,:,1])-1.0
-	a_max = np.max(lab[:,:,1])+1.0
-	b_min = np.min(lab[:,:,2])-1.0
-	b_max = np.max(lab[:,:,2])+1.0
-	lab[:,:,1] = (lab[:,:,1]-a_min)/(a_max-a_min)
-	lab[:,:,2] = (lab[:,:,2]-b_min)/(b_max-b_min)
+	lab[:,:,1] = (lab[:,:,1]-AB_MIN)/(AB_MAX - AB_MIN)
+	lab[:,:,2] = (lab[:,:,2]-AB_MIN)/(AB_MAX - AB_MIN)
 	return lab
-
-def read_scaled_color_image_Lab_extrema(filename):
-	# Read image, cut off alpha channel, only keep rgb.
-	rgb = io.imread(filename)[:,:,:3]
-	# Resize to 128x128 (temporary until we ensure inputs are 128x128)
-	rgb = misc.imresize(rgb, (128,128))
-	lab = color.rgb2lab(rgb).astype(np.float32)
-	# rescale a and b so that they are in the range (0,1) of the sigmoid function
-	a_min = np.min(lab[:,:,1])-1.0
-	a_max = np.max(lab[:,:,1])+1.0
-	b_min = np.min(lab[:,:,2])-1.0
-	b_max = np.max(lab[:,:,2])+1.0
-	lab[:,:,1] = (lab[:,:,1]-a_min)/(a_max-a_min)
-	lab[:,:,2] = (lab[:,:,2]-b_min)/(b_max-b_min)
-	return (lab, a_max, a_min, b_max, b_min)
-
 
 # Returns a one-hot vector given an image filename.
 # Used to get labels for the classification network.
@@ -509,8 +491,9 @@ def main(trainNetwork, inputFilename, outputFilename):
 		# Get training data, in batches.
 		train_dir = IMAGES_DIR + 'train/'
 		train_filenames = os.listdir(train_dir)
-		num_images = len(train_filenames)
-		assert(num_images == NUM_TRAIN_IMAGES)
+		num_images = NUM_TRAIN_IMAGES
+		# num_images = len(train_filenames)
+		# assert(num_images == NUM_TRAIN_IMAGES)
 		num_batches = int(math.ceil(float(num_images/BATCH_SIZE)))
 
 		for epoch in xrange(NUM_EPOCHS):
@@ -524,26 +507,17 @@ def main(trainNetwork, inputFilename, outputFilename):
 				class_labels = np.zeros([BATCH_SIZE, NUM_CLASSES])
 				# Determine where in the global list of files we should start for this batch.
 				start_idx = batch * BATCH_SIZE
-				# TEMPORARY: Keep track of max and min values for a and b.
-				a_max = -sys.maxsize - 1
-				b_max = -sys.maxsize - 1
-				a_min = sys.maxsize
-				b_min = sys.maxsize
 				for i in xrange(BATCH_SIZE):
         	                	# Get the next file to look at, based on precalculated random order.
 					file_idx = training_images_index[start_idx + i]
 					filename = train_filenames[file_idx]
 					label = one_hot_from_filename(filename)
 					class_labels[i] = label
-					im, amax, amin, bmax, bmin = read_scaled_color_image_Lab_extrema(train_dir + filename)
+					im = read_scaled_color_image_Lab(train_dir + filename)
 					im_bw = im[:,:,0].reshape((-1,IMAGE_SIZE,IMAGE_SIZE,1))
 					im_c = im[:,:,1:].reshape((-1,IMAGE_SIZE,IMAGE_SIZE,2))
 					bw_images[i] = im_bw
 					color_features[i] = im_c
-					a_max = max(a_max, amax)
-					a_min = min(a_min, amin)
-					b_max = max(b_max, bmax)
-					b_min = min(b_min, bmin)
 
 				y_downsample = tf.image.resize_nearest_neighbor(color_features, [IMAGE_SIZE/2, IMAGE_SIZE/2]).eval(session=sess)
 				feed_dict = {train_data_node: bw_images,
@@ -556,7 +530,6 @@ def main(trainNetwork, inputFilename, outputFilename):
 				# Update step count and save variables
 				step += 1
 	            		print('\tGlobal step: %d' % step)
-				print('\na_max %f\na_min %f\nb_max %f\nb_min %f' % (a_max, a_min, b_max, b_min))
 				if step%100 == 0:
 					print('Saving variables')
 					saver.save(sess, CKPT_DIR + '/model.ckpt', write_meta_graph=False)
@@ -590,28 +563,32 @@ def main(trainNetwork, inputFilename, outputFilename):
 		print 'Test error: %f' % test_error
         	print 'Saving'
 		saver.save(sess, CKPT_DIR + '/model.ckpt', write_meta_graph=False)
-		test_im = np.zeros([IMAGE_SIZE, IMAGE_SIZE, 3], dtype=np.float32)
+		'''test_im = np.zeros([IMAGE_SIZE, IMAGE_SIZE, 3], dtype=np.float32)
 		print test_data.shape
 		test_im[:,:,0] = np.squeeze(test_data[0]/255.0)
 		test_im[:,:,1:] = test_predictions[0,0]
 		io.imsave('output.png', color.lab2rgb(test_im))
+		'''
 	else:
 		# Evaluate input image and colorize it.
 		im = read_scaled_color_image_Lab(inputFilename)
 		
 		np.set_printoptions(precision=3, suppress=True)
+		'''
 		print('Original image')
 		print(im[:,:,0])
 		print('A component')
 		print(im[:,:,1])
+		'''
 		im_bw = im[:,:,0].reshape((1,IMAGE_SIZE,IMAGE_SIZE,1))
                 feed_dict = {test_input_image: im_bw}
 		res = np.array(sess.run(test_prediction, feed_dict = feed_dict))
+		res = res * (AB_MAX - AB_MIN) + AB_MIN
 		im[:,:,1:] = res
+		'''
 		print('\nNew A component')
 		print(im[:,:,1])
-		
-		im[:,:,0] = im[:,:,0]/255.0
+		'''
 		rgb = color.lab2rgb(im)
 		io.imsave(outputFilename, rgb)
 